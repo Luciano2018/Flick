ebd8e85147421750fa21adb265a09a71fb5e3bbe
Raspberry Pi Buenos Aires <38385411+Luciano2018@users.noreply.github.com>
2022-02-06 17:51:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,13 +10,10 @@ fi
 if [ ! $? = 0 ]; then
    exit 1
 else
-  #Installs packages which might be missing
-   apt-get install -y git whiptail
-
    # enable I2C
    raspi-config nonint do_i2c 0
 
-   git clone https://github.com/PiSupply/Flick.git
+   git clone https://github.com/Luciano2018/Flick.git
    cd Flick/flick
    python setup.py install
    whiptail --msgbox "The system will now reboot" 8 40
